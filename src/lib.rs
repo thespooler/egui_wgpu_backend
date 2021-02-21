100d908d009977f1cb7abc0f8c96187996a58710
TheSpooler <spooler@venrik.com>
2021-02-21 12:05:33 -0500

    Updated BlendState to merge color and alpha

--- src/lib.rs
+++ src/lib.rs
@@ -199,16 +199,18 @@ impl RenderPass {
                 entry_point: "main",
                 targets: &[wgpu::ColorTargetState {
                     format: output_format,
-                    color_blend: wgpu::BlendState {
+                    blend: Some(wgpu::BlendState {
+                        color: wgpu::BlendComponent {
                         src_factor: wgpu::BlendFactor::One,
                         dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha,
                         operation: wgpu::BlendOperation::Add,
                     },
-                    alpha_blend: wgpu::BlendState {
+                        alpha: wgpu::BlendComponent {
                         src_factor: wgpu::BlendFactor::OneMinusDstAlpha,
                         dst_factor: wgpu::BlendFactor::One,
                         operation: wgpu::BlendOperation::Add,
-                    },
+                        }
+                    }),
                     write_mask: wgpu::ColorWrite::ALL,
                 }],
             }),
